3f118b7b4a3590ee17d6511b5404166045123632
egi purwana <egipurwana@gmail.com>
2017-05-20 15:27:57 +0700

    mn

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -53,11 +53,11 @@ class Route
     public function register(\Slim\App $app)
     {
 	    $app->get('/',function(\Slim\Http\Request $req, \Slim\Http\Response $res) use ($app){
-			$this->$app->setCookie('foo', 'bar', '2 days');
+			//$this->$app->setCookie('foo', 'bar', '2 days');
 	    });
 	    $app->get('/training',function(\Slim\Http\Request $req, \Slim\Http\Response $res) use ($app){
-		    $cookies = $this->$app->getCookie('foo');
-		    echo $cookies;
+		    //$cookies = $this->$app->getCookie('foo');
+		    //echo $cookies;
 			//require_once(__DIR__ . '/../../../public/datatrain.php');			
 	    });
 		/*

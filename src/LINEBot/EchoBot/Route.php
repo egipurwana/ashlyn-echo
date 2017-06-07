552bdc6146f5134e0362989cadb39261156024f4
egi purwana <egipurwana@gmail.com>
2017-06-08 01:38:56 +0700

    make it

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -227,24 +227,16 @@ class Route
 								        $sql1 = "SELECT idanswer FROM relation where idphrase = '".$row["id"]."'";
 										$result1 = $conn->query($sql1);						
 										if ($result1->num_rows > 0) {
-											
-											/*$post = array();
-										    while($row = mysql_fetch_assoc($result1))
-										    {
-										        $post[] = $row;
-										    }*/
-											
+
 											while($row = $result1->fetch_array())
 											{
 												$rows[] = $row;
 											}
+											$intran = mt_rand(0,$result1->num_rows);
 											
-											//$rowwww = mysqli_fetch_array($result1,MYSQLI_NUM);
-											$srcs = print_r($rows, true);
-											
-											$resp = $bot->replyText($event->getReplyToken(),'terdapat '.$result1->num_rows.' jawaban '.$rows[0]['idanswer']);
-										    /*while($row1 = $result1->fetch_assoc()) {
-										        $sql2 = "SELECT * FROM answer where id = '".$row1["idanswer"]."'";
+											//$resp = $bot->replyText($event->getReplyToken(),'terdapat '.$result1->num_rows.' jawaban '.$rows[0]['idanswer']);
+										    while($row1 = $result1->fetch_assoc()) {
+										        $sql2 = "SELECT * FROM answer where id = '".$intran."'";//$row1["idanswer"]
 												$result2 = $conn->query($sql2);						
 												if ($result2->num_rows > 0) {
 												    while($row2 = $result2->fetch_assoc()) {
@@ -273,7 +265,7 @@ class Route
 												} else {
 													//not found
 												}
-										    }*/
+										    }
 										} else {
 											//not found
 										}

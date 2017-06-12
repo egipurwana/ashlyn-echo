ffa31c10d3c1ab08ba9e414de45496fdde685f1c
egi purwana <egipurwana@gmail.com>
2017-06-13 01:52:32 +0700

    oooh

--- src/LINEBot/EchoBot/Route.php
+++ src/LINEBot/EchoBot/Route.php
@@ -338,7 +338,7 @@ class Route
  						$responses = self::CallAPI("GET", "https://quark.timeshift.tech/imageSearch/imagesearch/api?url=https://g-search4.alicdn.com/bao/uploaded/i3/TB1ygnzHVXXXXcoXFXXXXXXXXXX_!!0-item_pic.jpg_240x240.jpg");
 
  						$ismatch = json_decode($responses);
-						$resp = $bot->replyText($event->getReplyToken(),  "match : ".$ismatch->{'is matched'}." nama : ".$ismatch->{'name'}." ".$event->getPackageId());
+						$resp = $bot->replyText($event->getReplyToken(),  "match : ".$ismatch->{'is matched'}." nama : ".$ismatch->{'name'}." ".$event->getPackageId()." ".$responsex);
 						//.$event->getPackageId()
 						
                     } elseif ($event instanceof AudioMessage) {
